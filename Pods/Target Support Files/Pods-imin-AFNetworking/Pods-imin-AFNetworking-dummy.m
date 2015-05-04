5b2d85531954aef4f6716aff9897bf4ac6d93ca6
Alexey Blinov <alexey.blinov@gmail.com>
2015-05-04 15:28:35 +0100

    Added missing Pods directory

---
+++ Pods/Target Support Files/Pods-imin-AFNetworking/Pods-imin-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_imin_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_imin_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-imin-SwiftyJSON/Pods-imin-SwiftyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_imin_SwiftyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_imin_SwiftyJSON
+@end
